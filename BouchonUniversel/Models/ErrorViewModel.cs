d3b0b0e78a5a96716313b1c7e597316088d5f05a
Romain AVONDE <romain.avonde@capgemini.com>
2018-04-04 18:18:10 +0200

    Ajoutez des fichiers projet.

---
+++ BouchonUniversel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BouchonUniversel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
