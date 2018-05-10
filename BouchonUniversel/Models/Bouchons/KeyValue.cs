a0c55f7afa5b89d2e6baacbcc8ec146d080ef02d
Romain AVONDE <romain.avonde@capgemini.com>
2018-05-10 19:39:13 +0200

    Ajout de la réponse bouchonnée

---
+++ BouchonUniversel/Models/Bouchons/KeyValue.cs
@@ -0,0 +1,16 @@
+﻿namespace BouchonUniversel.Models.Bouchons
+{
+    using JetBrains.Annotations;
+
+    [UsedImplicitly]
+    public class KeyValue
+    {
+        #region Propriétés et indexeurs
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        #endregion
+    }
+}
---
+++ BouchonUniversel/Models/Bouchons/ReponseBouchonnee.cs
@@ -0,0 +1,25 @@
+﻿namespace BouchonUniversel.Models.Bouchons
+{
+    #region Usings
+
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+
+    #endregion
+
+    [UsedImplicitly]
+    public class ReponseBouchonnee
+    {
+        #region Propriétés et indexeurs
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public string Body { get; set; }
+
+        public string Query { get; set; }
+
+        public string Route { get; set; }
+
+        #endregion
+    }
+}
