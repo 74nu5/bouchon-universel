5c3ce591c23bf31d12d37a9eb452833311e1e94c
Romain AVONDE <romain.avonde@capgemini.com>
2018-05-25 15:37:25 +0200

    Fix #1 Amélioration de la gestion  des erreurs dans le controller

---
+++ BouchonUniversel/Models/Bouchons/ResponseErreur.cs
@@ -0,0 +1,20 @@
+﻿namespace BouchonUniversel.Models.Bouchons
+{
+    /// <summary>The response erreur.</summary>
+    public class ResponseErreur
+    {
+        #region Propriétés et indexeurs
+
+        /// <summary>Gets or sets the message.</summary>
+        public string Message { private get; set; }
+
+        /// <summary>Gets or sets the code.</summary>
+        public int Code { private get; set; }
+
+        /// <summary>The code message.</summary>
+        public object CodeMessage
+            => $"{this.Code} - {this.Message}";
+
+        #endregion
+    }
+}
